45149922e80878f41eb3c2f229f52c16934b88ba
duaqader <99490467+duaqader@users.noreply.github.com>
2024-06-27 18:46:10 +0300

    Update ft_putstr.c

--- ft_putstr.c
+++ ft_putstr.c
@@ -5,14 +5,26 @@
 Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
+#include <unistd.h>
+#include <stdio.h>
 
 void ft_putstr(char *str)
 {
-    unsighned int i;
+    unsigned int i;
+    
     i=0;
     while (str[i] != '\0')
     {
         write(1, &str[i],1);
         i++;
     }
+}
+
+int main() {
+    char str[] = "Hello, World!";
+    
+    // Print the string using ft_putstr
+    ft_putstr(str);
+    
+    return 0;
 }
