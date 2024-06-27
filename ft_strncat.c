3323edf6e84994f700c58e51efb918d633938813
duaqader <99490467+duaqader@users.noreply.github.com>
2024-06-27 20:41:27 +0300

    Update ft_strncat.c

--- ft_strncat.c
+++ ft_strncat.c
@@ -29,16 +29,52 @@ char *ft_strncat(char *dest, char *src, unsigned int nb)
     return (dest);
 }
 
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+char *ft_strncat(char *dest, char *src, unsigned int nb)
+{
+    unsigned int i = 0;
+    unsigned int j = 0;
+    
+ 
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+    while (src[j] != '\0' && j < nb)
+    {
+        dest[i] += src[j];
+        i++;
+        j++;
+    }
+    dest[i]= '\0'; 
+    return (dest);
+}
+
 int main() {
-    char dest[50] = "Hello, "; // Ensure dest has enough space to hold the concatenated result
+    char dest1[50] = "Hello, "; // Ensure dest1 has enough space to hold the concatenated result
+    char dest2[50] = "Hello, "; // Separate buffer for standard strncat
     char src[] = "World!";
     unsigned int nb = 3;
     
-    // Concatenate src to dest up to nb characters
-    ft_strncat(dest, src, nb);
+    // Concatenate src to dest1 up to nb characters using ft_strncat
+    ft_strncat(dest1, src, nb);
     
-    // Print the concatenated string
-    printf("Concatenated string: %s\n", dest);
+    // Concatenate src to dest2 up to nb characters using standard strncat
+    strncat(dest2, src, nb);
+    
+    // Print the results
+    printf("Result using ft_strncat: %s\n", dest1);
+    printf("Result using standard strncat: %s\n", dest2);
     
     return 0;
 }
